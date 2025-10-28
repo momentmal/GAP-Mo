5047ddeda77ab7c204dd7918cf2132126384e223
Moritz Weller <moritzweller@hotmail.com>
2025-10-28 02:09:06 +0100

    local https adaptions

--- geo_activity_playground/webui/app.py
+++ geo_activity_playground/webui/app.py
@@ -112,6 +112,10 @@ def web_ui_main(
         "ignore", r"datetime.datetime.utcfromtimestamp\(\) is deprecated"
     )
     app = Flask(__name__)
+    
+    #modification for local VR rendering
+    from flask_cors import CORS
+    CORS(app)  # allow all origins
 
     database_path = pathlib.Path("database.sqlite")
     logger.info(f"Using database file at '{database_path.absolute()}'.")
@@ -290,7 +294,15 @@ def web_ui_main(
         )
         return variables
 
-    app.run(host=host, port=port)
+        #mod for local VR rendering
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        ssl_context=(
+            "C:/Dropbox/apps/_scripts/chatgpt/threejs-webXR-map/cert.pem",
+            "C:/Dropbox/apps/_scripts/chatgpt/threejs-webXR-map/key.pem",
+        )
+    )
 
 
 def _try_get_version():
